f3794bc5b4af023d0b8373c2f1be6b4203e276e7
dustinheestand <heestand@post.harvard.edu>
2020-06-13 17:01:23 -0700

    go mod tidy

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -22,14 +31,17 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	router := gin.New()
-	router.Use(gin.Logger())
+	e.Static("/static", "static")
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.tmpl.html")),
+	}
+	e.Renderer = t
 	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	e.GET("/", func(ctx echo.Context) error {
+		return c.Render(http.StatusOK, "")
+		ctx.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.Run(":" + port)
+	e.Logger.Fatal(e.Start(":" + port))
 }
